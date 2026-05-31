d7b608c32c6fa7236b79c668a22604857da5d97d
Jheneral1 <2425403870@live.gctu.edu.gh>
2026-05-31 08:34:20 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Prints stylized large initials using only print statements
+# Assignment Number: 1
+#
+# Name: ALEXANDER ASOPIAH
+# STUDENT ID: 2425403870
+# Email: 2425403870@live.gctu.edu.gh
+# Grader: MR.AUGUSTUS
+#
+# On my honor, Alexander Asopiah, this program is my own work.
+#and i have not provided this code to any other student.
+
+print()
 
 def main():
-  pass #code goes here
+    # This program prints my initials
 
+    print()
+    print("...AAA")
+    print()
+
+    print(".......AAAA................AAAA................AAAA.........")
+    print(".....AAA..AAA............AAA..AAA............AAA..AAA.......")
+    print("....AAA....AAA..........AAA....AAA..........AAA....AAA......")
+    print("...AAA......AAA........AAA......AAA........AAA......AAA.....")
+    print("...AAA......AAA........AAA......AAA........AAA......AAA.....")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...AAA......AAA........AAA......AAA........AAA......AAA.....")
+    print("...AAA......AAA........AAA......AAA........AAA......AAA.....")
+    print("...AAA......AAA...**...AAA......AAA...**...AAA......AAA...**")
+    print("...AAA......AAA...**...AAA......AAA...**...AAA......AAA...**")
+    print()
 
 
 main()
